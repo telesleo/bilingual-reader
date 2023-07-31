b65be74ac5447e2a3e068db794297cae1c894968
leo <leonardoandrei41@gmail.com>
2023-07-31 00:06:06 -0300

    Modify Reader page to display text as sentences

--- src/pages/Reader/index.jsx
+++ src/pages/Reader/index.jsx
@@ -3,6 +3,8 @@ import styles from './reader.module.css';
 
 export default function Reader() {
   const [text, setText] = useState('');
+  const [sentences, setSentences] = useState([]);
+  const [highlightedSentence, setHighlightedSentence] = useState(-1);
   const [voices, setVoices] = useState([]);
   const [selectedVoice, setSelectedVoice] = useState();
 
@@ -22,6 +24,16 @@ export default function Reader() {
     };
   }, []);
 
+  useEffect(() => {
+    let taggedText = text;
+
+    taggedText = taggedText.replace(/\./g, '.<end>');
+    taggedText = taggedText.replace(/,/g, '.<end>');
+    taggedText = taggedText.replace(/;/g, '.<end>');
+
+    setSentences(taggedText.split(/<end>/g));
+  }, [text]);
+
   const speak = () => {
     const utterance = new SpeechSynthesisUtterance(text);
     utterance.voice = selectedVoice;
@@ -30,10 +42,40 @@ export default function Reader() {
 
   return (
     <div>
-      <input type="text" value={text} onChange={({ target }) => setText(target.value)} />
+      <textarea type="text" value={text} onChange={({ target }) => setText(target.value)} />
       <div id={styles['text-container']}>
-        <p translate="no" className="notranslate">{text}</p>
-        <p>{text}</p>
+        <p translate="no" className="notranslate">
+          {
+            sentences.map((sentence, index) => (
+              <>
+                <span
+                  onMouseEnter={() => setHighlightedSentence(index)}
+                  onMouseLeave={() => setHighlightedSentence(-1)}
+                  className={`${styles.sentence}${(highlightedSentence === index) ? ` ${styles['highlighted-text']}` : ''}`}
+                >
+                  {sentence}
+                </span>
+                {index !== sentences.length - 1 && ' '}
+              </>
+            ))
+          }
+        </p>
+        <p>
+          {
+            sentences.map((sentence, index) => (
+              <>
+                <span
+                  onMouseEnter={() => setHighlightedSentence(index)}
+                  onMouseLeave={() => setHighlightedSentence(-1)}
+                  className={`${styles.sentence}${(highlightedSentence === index) ? ` ${styles['highlighted-text']}` : ''}`}
+                >
+                  {sentence}
+                </span>
+                {index !== sentences.length - 1 && ' '}
+              </>
+            ))
+          }
+        </p>
       </div>
       <select onChange={({ target }) => setSelectedVoice(voices[target.value])}>
         {
